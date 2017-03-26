7a2725f090e191762a3f95c7c91f7977472e9a04
root <account-login@users.noreply.github.com>
2017-03-26 21:05:44 +0800

    rewrite CachingResolver.

--- dnsagent/resolver/cache.py
+++ dnsagent/resolver/cache.py
@@ -1,4 +1,7 @@
-import itertools
+import abc
+from collections import OrderedDict
+from itertools import chain
+import math
 
 from twisted.internet import defer
 from twisted.names import dns
@@ -11,94 +14,181 @@ from dnsagent.utils import get_reactor
 __all__ = ('CachingResolver',)
 
 
+class BaseCachePolicy(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def touch(self, key):
+        pass
+
+    @abc.abstractmethod
+    def remove(self, key):
+        pass
+
+    @abc.abstractmethod
+    def clear(self):
+        pass
+
+
+class UnlimitedPolicy(BaseCachePolicy):
+    def touch(self, key):
+        pass
+
+    def remove(self, key):
+        pass
+
+    def clear(self):
+        pass
+
+
+class LRUPolicy(BaseCachePolicy):
+    def __init__(self, maxsize):
+        assert maxsize > 0
+        self.maxsize = maxsize
+        self.od = OrderedDict()
+
+    def touch(self, key):
+        self.od.setdefault(key)
+        self.od.move_to_end(key)
+        if len(self.od) > self.maxsize:
+            return self.od.popitem(last=False)[0]
+
+    def remove(self, key):
+        del self.od[key]
+
+    def clear(self):
+        self.od.clear()
+
+
+class TTLCache:
+    def __init__(self, clean_inteval=30, policy: BaseCachePolicy = None, reactor=None):
+        self.clean_inteval = clean_inteval
+        self.policy = policy or UnlimitedPolicy()
+        self.reactor = get_reactor(reactor)
+
+        self.map = dict()
+        self.started_time = None    # type: float
+        self.pending_clean = dict()
+        self.delayed_calls = dict()
+
+    def put(self, key, value, ttl: float):
+        assert key is not None
+        assert ttl > 0
+
+        evicted = self.policy.touch(key)
+        if evicted is not None:
+            del self.map[evicted]
+
+        now = self.reactor.seconds()
+        if self.started_time is None:
+            self.started_time = now
+
+        expire_time = now + ttl
+        self.map[key] = value, expire_time
+
+        tick = self._get_tick(expire_time)
+        if tick not in self.pending_clean:
+            assert tick not in self.delayed_calls
+            clean_time = self.started_time + tick * self.clean_inteval
+            dc = self.reactor.callLater(
+                # add a small delay to clean time to avoid firing self.run_clean() too early
+                clean_time - now + 0.01,
+                self._run_clean_up, tick,
+            )
+            self.delayed_calls[tick] = dc
+        pending = self.pending_clean.setdefault(tick, [])
+        pending.append(key)
+
+    def get(self, key):
+        value, expire_time = self.map[key]  # raise KeyError
+        self.policy.touch(key)
+        now = self.reactor.seconds()
+        if expire_time < now:
+            self.remove(key)
+            raise KeyError
+
+        return value, expire_time - now
+
+    def peek(self, key):
+        value, expire_time = self.map[key]  # raise KeyError
+        return value, expire_time - self.reactor.seconds()
+
+    def remove(self, key):
+        del self.map[key]
+        self.policy.remove(key)
+
+    def __len__(self):
+        return len(self.map)
+
+    def _get_tick(self, time):
+        return math.ceil((time - self.started_time) / self.clean_inteval)
+
+    def _run_clean_up(self, tick):
+        del self.delayed_calls[tick]
+        now = self.reactor.seconds()
+        for key in self.pending_clean.pop(tick):
+            try:
+                value, expire_time = self.map[key]
+            except KeyError:
+                pass
+            else:
+                if expire_time <= now:
+                    self.remove(key)
+                else:
+                    assert key in self.pending_clean[self._get_tick(expire_time)]
+
+    def clear(self):
+        self.map.clear()
+        self.policy.clear()
+        self.pending_clean.clear()
+        # we need cancel all DelayedCall in unittest
+        for dc in self.delayed_calls.values():
+            dc.cancel()
+        self.delayed_calls.clear()
+
+
+def rrheader_update_ttl(rr: dns.RRHeader, new_ttl):
+    return dns.RRHeader(
+        name=rr.name.name, type=rr.type, cls=rr.cls, ttl=int(new_ttl),
+        payload=rr.payload,
+    )
+
+
 class CachingResolver(BaseResolver):
     """
     A resolver that caches the output of another resolver.
 
     ref: twisted.names.cache.CacheResolver
     """
-    def __init__(self, resolver, reactor=None):
+    def __init__(self, resolver, cache=None, reactor=None):
         super().__init__()
 
         self.resolver = resolver
         self.reactor = get_reactor(reactor)
-        self.cache = dict()
-        self.cancel = dict()
+        self.cache = cache or TTLCache(reactor=self.reactor)
 
+    @defer.inlineCallbacks
     def _lookup(self, name, cls, type_, timeout, **kwargs):
         # TODO: queue identical query
         request_id = kwargs.get('request_id', -1)
 
-        def cache_miss(query):
-            logger.debug('[%d]cache miss: %s', request_id, name.decode('latin1'))
-
-            def add_to_cache(res):
-                self.cache_result(query, res, **kwargs)
-                return res
-
-            d = self.resolver.query(query, timeout=timeout, **kwargs)
-            return d.addCallback(add_to_cache)
-
-        def adjust_ttl(rr: dns.RRHeader, diff):
-            return dns.RRHeader(
-                name=rr.name.name, type=rr.type, cls=rr.cls, ttl=int(rr.ttl - diff),
-                payload=rr.payload,
-            )
-
-        q = dns.Query(name, type_, cls)
+        query = dns.Query(name, type_, cls)
         try:
-            when, (ans, auth, add) = self.cache[q]
+            result, ttl = self.cache.get(query)
         except KeyError:
-            return cache_miss(q)
+            logger.debug('[%d]cache miss: %s', request_id, name.decode('latin1'))
+            result = yield self.resolver.query(query, timeout=timeout, **kwargs)
+
+            logger.debug('[%d]adding to cache: %r', kwargs.get('request_id', -1), query)
+            minttl = min((rr.ttl for rr in chain.from_iterable(result)), default=0)
+            if minttl > 0:
+                self.cache.put(query, result, minttl)
+            return result
         else:
-            now = self.reactor.seconds()
-            diff = now - when
-            try:
-                result = (
-                    [adjust_ttl(r, diff) for r in ans],
-                    [adjust_ttl(r, diff) for r in auth],
-                    [adjust_ttl(r, diff) for r in add],
-                )
-            except ValueError:
-                # negative ttl
-                return cache_miss(q)
-            else:
-                logger.debug('[%d]cache hit: %s', request_id, name.decode('latin1'))
-                return defer.succeed(result)
-
-    def cache_result(self, query, payload, cache_time=None, **kwargs):
-        """
-        Cache a DNS entry.
-
-        @param query: a L{dns.Query} instance.
-        @param payload: a 3-tuple of lists of L{dns.RRHeader} records, the
-            matching result of the query (answers, authority and additional).
-        @param cache_time: The time (seconds since epoch) at which the entry is
-            considered to have been added to the cache. If L{None} is given,
-            the current time is used.
-        """
-        minttl = min(
-            map(lambda rr: rr.ttl, itertools.chain.from_iterable(payload)),
-            default=0,
-        )
-
-        logger.debug('[%d]adding to cache: %r', kwargs.get('request_id', -1), query)
-        self.cache[query] = (cache_time or self.reactor.seconds(), payload)
-
-        if query in self.cancel:
-            # reset count down
-            self.cancel[query].cancel()
-        self.cancel[query] = self.reactor.callLater(minttl, self.clear_entry, query)
-
-    def clear_entry(self, query):
-        del self.cache[query]
-        del self.cancel[query]
+            assert ttl >= 0
+            logger.debug('[%d]cache hit: %s', request_id, name.decode('latin1'))
+            return tuple([rrheader_update_ttl(r, ttl) for r in rrlist] for rrlist in result)
 
     def clear(self):
-        for d in self.cancel.values():
-            d.cancel()
-        for query in list(self.cancel.keys()):
-            self.clear_entry(query)
+        self.cache.clear()
 
     def __repr__(self):
         return '<Cache for={}>'.format(self.resolver.__class__.__name__)
--- dnsagent/tests/test_caching_resolver.py
+++ dnsagent/tests/test_caching_resolver.py
@@ -1,11 +1,146 @@
 from ipaddress import IPv4Address
 
-from twisted.internet import task
+import pytest
+from twisted.internet import task, defer
 from twisted.names import dns
+from twisted.trial import unittest
 
-from dnsagent.resolver import CachingResolver
+from dnsagent.resolver.cache import LRUPolicy, TTLCache, CachingResolver
 from dnsagent.tests import BaseTestResolver, FakeResolver, iplist
 from dnsagent.utils import rrheader_to_ip, sequence_deferred_call
+
+
+class TestLRUPolicy(unittest.TestCase):
+    def setUp(self):
+        self.policy = LRUPolicy(3)
+
+    def assert_items(self, items):
+        assert list(self.policy.od.keys()) == items
+
+    def test_evict(self):
+        assert self.policy.touch(1) is None
+        assert self.policy.touch(2) is None
+        assert self.policy.touch(1) is None
+        assert self.policy.touch(3) is None
+        assert self.policy.touch(4) == 2
+        self.assert_items([1, 3, 4])
+
+    def test_remove(self):
+        self.policy.touch(1)
+        self.policy.touch(2)
+        self.policy.touch(3)
+
+        self.policy.remove(2)
+        self.assert_items([1, 3])
+
+
+class TestTTLCache(unittest.TestCase):
+    def setUp(self):
+        self.clock = task.Clock()
+        self.cache = TTLCache(clean_inteval=30, reactor=self.clock)
+
+    def tearDown(self):
+        assert not self.clock.getDelayedCalls()
+
+    def test_put_get_expire(self):
+        self.cache.put('key', 'value', 10)
+        assert self.cache.pending_clean[1] == ['key']
+        assert len(self.cache.delayed_calls) == 1
+
+        self.clock.advance(9)
+        assert self.cache.get('key') == ('value', 1)
+        self.clock.advance(2)
+        with pytest.raises(KeyError):
+            self.cache.get('key')
+
+        self.clock.advance(20)
+        assert not self.cache.pending_clean
+        assert not self.cache.delayed_calls
+
+    def test_put_clean_get(self):
+        self.cache.put('k1', 'v1', 10)
+        self.cache.put('k2', 'v2', 20)
+
+        self.clock.advance(31)
+        with pytest.raises(KeyError):
+            self.cache.get('k1')
+        with pytest.raises(KeyError):
+            self.cache.get('k2')
+
+        assert not self.cache.pending_clean
+        assert not self.cache.delayed_calls
+
+    def test_ttl_increase(self):
+        self.cache.put('key', 'v1', 10)
+        self.cache.put('key', 'v2', 40)
+
+        self.clock.advance(9)
+        assert self.cache.get('key') == ('v2', 31)
+        self.clock.advance(30)
+        assert self.cache.get('key') == ('v2', 1)
+
+        self.clock.advance(22)
+        with pytest.raises(KeyError):
+            self.cache.get('key')
+
+    def test_ttl_decrease(self):
+        self.cache.put('key', 'v1', 40)
+        self.cache.put('key', 'v2', 10)
+
+        self.clock.advance(31)
+        with pytest.raises(KeyError):
+            self.cache.get('key')
+
+        self.clock.advance(30)
+
+    def test_clean(self):
+        self.cache.put('k1', 'v1', 20)
+        self.cache.put('k2', 'v2', 40)
+        self.cache.clear()
+
+        assert not self.cache.map
+        assert not self.cache.pending_clean
+        assert not self.cache.delayed_calls
+
+    def test_len(self):
+        assert len(self.cache) == 0
+        self.cache.put('key', 'v1', 100)
+        self.cache.put('key', 'v2', 100)
+        assert len(self.cache) == 1
+
+        self.cache.clear()
+
+
+class TestTTLCacheWithLRUPolicy(unittest.TestCase):
+    def setUp(self):
+        self.policy = LRUPolicy(3)
+        self.clock = task.Clock()
+        self.cache = TTLCache(clean_inteval=30, policy=self.policy, reactor=self.clock)
+
+    def tearDown(self):
+        assert not self.cache.policy.od
+        assert not self.cache.map
+
+    def check_state(self, items):
+        assert list(self.policy.od.keys()) == items
+        assert set(self.cache.map.keys()) == set(items)
+
+    def test_run(self):
+        self.cache.put('k0', 'v0', 500)
+        self.cache.put('k1', 'v1', 10)
+        self.cache.put('k3', 'v3', 20)
+        self.cache.put('k2', 'v2', 100)
+        self.cache.put('k3', 'v3', 20)
+        self.check_state(['k1', 'k2', 'k3'])
+
+        self.cache.get('k2')
+        self.check_state(['k1', 'k3', 'k2'])
+
+        self.clock.advance(31)
+        self.check_state(['k2'])
+
+        self.cache.remove('k2')
+        self.check_state([])
 
 
 class TestCachingResolver(BaseTestResolver):
@@ -24,32 +159,33 @@ class TestCachingResolver(BaseTestResolver):
         pass
 
     def test_caching(self):
-        # TODO: test ttl
-        self.fake_resolver.set_answer('asdf', '0.0.0.1', ttl=30)
+        self.fake_resolver.set_answer('asdf', '0.0.0.1', ttl=25)
 
         def check_cached():
-            when, (ans, ns, add) = self.resolver.cache[dns.Query(b'asdf', dns.A, dns.IN)]
+            self.clock.advance(2)
+            (ans, ns, add), ttl = self.resolver.cache.peek(dns.Query(b'asdf', dns.A, dns.IN))
+            assert ttl == 25 - 2
             assert rrheader_to_ip(ans[0]) == IPv4Address('0.0.0.1')
-            assert len(self.resolver.cancel) == 1
 
             self.fake_resolver.set_answer('asdf', '0.0.0.2', ttl=30)
             # serve from cached
             return self.check_a('asdf', iplist('0.0.0.1'))
 
-        def check_expired_1():
+        def check_expired():
             self.clock.advance(31)
             return self.check_a('asdf', iplist('0.0.0.2'))
-
-        def check_expired_2():
-            self.clock.advance(31)
-            assert len(self.resolver.cache) == len(self.resolver.cancel) == 0
 
         return sequence_deferred_call([
             lambda: self.check_a('asdf', iplist('0.0.0.1')),
             check_cached,
-            check_expired_1,
-            check_expired_2,
+            check_expired,
         ])
+
+    @defer.inlineCallbacks
+    def test_zero_ttl(self):
+        self.fake_resolver.set_answer('asdf', '1.2.3.4', ttl=0)
+        yield self.check_a('asdf', iplist('1.2.3.4'))
+        assert len(self.resolver.cache) == 0
 
 
 del BaseTestResolver
